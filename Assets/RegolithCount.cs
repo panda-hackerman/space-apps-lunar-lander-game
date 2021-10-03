ba0555ded15fbe2207953384e807998b25a72c44
panda-hackerman <58387096+panda-hackerman@users.noreply.github.com>
2021-10-03 12:08:35 -0700

    Regolith Extraction and Counter

---
+++ Assets/RegolithCount.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class RegolithCount : MonoBehaviour
+{
+    public TMP_Text regolithScore;
+    private float regolithCount = 0f;
+
+    public void CollectRegolith()
+    {
+        regolithCount++;
+        regolithScore.text = $"Samples: {regolithCount}";
+    }
+}
---
+++ Assets/Scripts/Interactables/ExtractionPointInteractable.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExtractionPointInteractable : MonoBehaviour, IInteractable
+{
+    public void OnInteract()
+    {
+        Debug.Log("You extracted this point");
+    }
+}
