a9fe456ea74c6930504d5ca468e3ae530f4a2646
panda-hackerman <58387096+panda-hackerman@users.noreply.github.com>
2021-10-03 15:27:22 -0700

    Greenhouse interactable
    You can interact with the greenhouse to add plants

---
+++ Assets/Greenhouse.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Greenhouse : MonoBehaviour, IInteractable
+{
+    public int plantLevel = 0;
+    public GameObject[] plants1, plants2, plants3;
+    public RegolithCount player;
+    public SceneSwitcher sceneSwitcher;
+
+    private void Start()
+    {
+        foreach (var plant in plants1)
+        {
+            plant.SetActive(false);
+        }
+        foreach (var plant in plants2)
+        {
+            plant.SetActive(false);
+        }
+        foreach (var plant in plants3)
+        {
+            plant.SetActive(false);
+        }
+    }
+
+    public void UpdatePlants()
+    {
+        if (plantLevel == 1)
+        {
+            foreach (var plant in plants1)
+            {
+                plant.SetActive(true);
+            }
+        }
+        else if (plantLevel == 2)
+        {
+            foreach (var plant in plants1)
+            {
+                plant.SetActive(true);
+            }
+            foreach (var plant in plants2)
+            {
+                plant.SetActive(true);
+            }
+        }
+        else if (plantLevel == 3)
+        {
+            foreach (var plant in plants1)
+            {
+                plant.SetActive(true);
+            }
+            foreach (var plant in plants2)
+            {
+                plant.SetActive(true);
+            }
+            foreach (var plant in plants3)
+            {
+                plant.SetActive(true);
+            }
+
+            sceneSwitcher.LoadScene("WinScreen");
+        }
+    }
+
+    public void OnInteract()
+    {
+        plantLevel = player.regolithCount;
+        UpdatePlants();
+    }
+}
