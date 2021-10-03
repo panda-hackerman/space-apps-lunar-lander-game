f78f6536d5890f55298087046bf67c2b2a249a95
panda-hackerman <58387096+panda-hackerman@users.noreply.github.com>
2021-10-03 00:20:07 -0700

    Skybox and Oxygen
    Fixed skybox and made it so you can refill oxygen at the lander

---
+++ Assets/Scripts/Interactables/LanderInteractable.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LanderInteractable : MonoBehaviour, IInteractable
+{
+    public PlayerOxygen playerOxygen;
+
+    public void OnInteract()
+    {
+        playerOxygen.RefillOxygen();
+    }
+}
