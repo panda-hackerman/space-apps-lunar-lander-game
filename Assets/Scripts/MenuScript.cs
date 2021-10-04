0b0716285faba10cdb922e9391dcf15c1f688b82
panda-hackerman <58387096+panda-hackerman@users.noreply.github.com>
2021-10-03 19:51:33 -0700

    Credits and buttons
    Added credits (assets need to be filled in still)
    Fixed main menu buttons and sprite

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -5,8 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour
 {
+    public GameObject CreditsPanel;
+
     public void StartGame()
     {
         SceneManager.LoadScene("MainScene");
+    }
+
+    public void OpenCredits()
+    {
+        CreditsPanel.SetActive(true);
+    }
+
+    public void CloseCredits()
+    {
+        CreditsPanel.SetActive(false);
     }
 }
