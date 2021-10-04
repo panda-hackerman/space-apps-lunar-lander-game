ae6bb62fb33225813e7b88b3cc1f7e7f97efe897
panda-hackerman <58387096+panda-hackerman@users.noreply.github.com>
2021-10-03 22:20:32 -0700

    Fixed player minimap icon

---
+++ Assets/Scripts/PlayerMapicon.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMapicon : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offset = new Vector3(0, 20f, 0);
+    public float rotOffset = 180;
+
+    private void Update()
+    {
+        transform.position = player.transform.position + offset;
+        transform.eulerAngles = transform.eulerAngles.Y(player.eulerAngles.y + rotOffset);
+    }
+}
